f9255eb499f92138379b7d26e536fe0522ec7e35
Marcus Lee <marcusslee95@berkeley.edu>
2020-12-17 11:23:55 -0800

    Applied styling to my intro page component by using CSS modules

--- just-another-blog-frontend-react/src/smartComponents/Intro.jsx
+++ just-another-blog-frontend-react/src/smartComponents/Intro.jsx
@@ -1,15 +1,16 @@
-function Intro() {
+import Intro from './Intro.module.css';
+
+const intro = () => {
     return (
-      
-      <div className="Intro">
+    <div className={Intro.AppliesToWholeComponent}>
         Hi there Stranger! <br/>
-        Congrats on being the 369th unique visit to this website! <br/>
+        Congrats on being the <b>369th</b> unique visit to this website! <br/><br/>
         Keep pumping those numbers up as you engage with the content 
         in this blog which covers programming, career advice, 
         relationships, thoughts, and everything in between! <br/>
         I'll see you around :) 
-      </div>
+    </div>
     );
   }
   
-  export default Intro;
+  export default intro;
