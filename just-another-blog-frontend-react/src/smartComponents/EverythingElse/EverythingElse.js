60a5bcf837c8ae4b38fc4b474ff4075a26b76be7
Marcus Lee <marcusslee95@berkeley.edu>
2020-12-20 01:23:08 -0800

    added intro text to everything else section. Also updated intro text for software engineering section

--- just-another-blog-frontend-react/src/smartComponents/EverythingElse/EverythingElse.js
+++ just-another-blog-frontend-react/src/smartComponents/EverythingElse/EverythingElse.js
@@ -5,6 +5,37 @@ class EverythingElse extends Component {
     }
 
     render () {
+        const introText = (
+            <p
+            style=
+            {{
+                textAlign: "left"
+            }}
+            >
+            I have lots of thoughts! As I'm sure you do too!<br/>
+            I find that writing them down is useful in several ways.<br/>
+            Namely, the activity of writing down your thoughts takes 
+            all the thoughts swirling in your head – in all it's mess -  
+            and gives it structure / organization.<br/>
+            This effect seems to be more pronounced when writing for an audience.<br/>
+            It also gives you a fun record to look back at.<br/>
+            The logic being you change over time. That change includes your thoughts.<br/>
+            Record your thoughts across a lifetime and you get to see how they progressed 
+            - in all their twists and turns - throughout your lifetime.<br/>
+            <br/>
+            So really, what I'm saying is... just being perfectly honest with you, this might 
+            be more of an activity for me than for you.😅<br/>
+            But hope you get something out of it too! Because unless we lived the exact same lives, <br/>
+            I'm pretty sure our different experiences 
+            would lead to us having different thoughts that were molded by those experiences.<br/>
+            <br/>
+            So let's get it started! Shall we?<br/>
+            On the right you can pick either the chronological or topical option 
+            to start browsing through my head (or what was once in it at least).<br/>
+            Just promise me you won't get lost in there OK? 😆
+            </p>
+        );
+
         return (
         <div
         style=
@@ -12,7 +43,7 @@ class EverythingElse extends Component {
                 color: 'white'
             }}
         >
-            EverythingElse Stuff
+            {introText}
         </div>
         );
         
--- just-another-blog-frontend-react/src/smartComponents/SoftwareEngineering/SoftwareEngineering.js
+++ just-another-blog-frontend-react/src/smartComponents/SoftwareEngineering/SoftwareEngineering.js
@@ -6,7 +6,7 @@ class SoftwareEngineering extends Component {
 
     render () {
         const introText = (
-            <h4
+            <p
             style=
             {{
                 textAlign: "left"
@@ -14,7 +14,7 @@ class SoftwareEngineering extends Component {
             >
             So… more interested in becoming a master at Googling and 
             copy pasting code from StackOverflow than everything else, huh?<br/>
-            Well I don't blame you! All joking aside I'm glad you came :-)<br/>
+            Well I don't blame you! All joking aside I'm glad you came 😀<br/>
             <br/>
             With a new front end framework seemingly popping up every other year,<br/>
             the classic "internships asking for ___ years of experience but 
@@ -23,8 +23,10 @@ class SoftwareEngineering extends Component {
             better describe an entire business unit than an actual person,<br/>
             and supply skyrocketing with astronomical increases in 
             aspiring Computer Science majors and Boot Camp grads…<br/>
-            The world of software engineering seems as hard to navigate as it has ever been. 
-            It might feel a bit daunting to do it alone.<br/>
+            The world of software engineering seems as competitive and 
+            hard to navigate as it has ever been.<br/>
+            It might feel a bit daunting to do it alone 
+            (I know it was and still is for me).<br/>
             <br/>
             Now if I were to tell you I'd be your best navigator, I'd be lying through my teeth.<br/>
             But what I can do is share with you what I've learned along the way, 
@@ -32,14 +34,16 @@ class SoftwareEngineering extends Component {
             <br/>
             And of course if I'm saying stuff you know is bogus... call me out on it!<br/>
             I'd like to think of us more as vertices connected with a bidirectional edge in a graph 
-            than a parent node connected to a child no in a tree.<br/>
-            You get my flow?<br/>
+            than a parent node connected to a child node in a tree.<br/>
+            You get my flow?😏<br/>
             <br/>
-            On the right-hand side you'll see a list of topics of which some of you might be interested in.<br/>
-            Click on one to see all the posts that fall under that topic.<br/>
+            On the right-hand side you'll see a list of topics of which some you might be interested in.<br/>
+            Click on a topic to see all the posts that fall under it.<br/>
             If you can't make up your mind, just click the by-date option 
-            to start with my most recent posts!
-            </h4>
+            to start with my most recent posts!<br/>
+            <br/>
+            Hope you find something you like!
+            </p>
         );
         return (
         <div
