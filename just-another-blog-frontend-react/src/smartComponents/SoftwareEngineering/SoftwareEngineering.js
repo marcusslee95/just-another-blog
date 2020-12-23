5f21011f016aa13cba9248797326455595e48757
Marcus Lee <marcusslee95@berkeley.edu>
2020-12-22 17:41:53 -0800

    change front end behavior so that instead of always necessarily showing the articles upon rendering the software engineering page I only show articles when the user has clicked a by date or by topic option. Also changed the contact list to show stuff like 2021 2020 or back and development front end development depending on which option was chosen

--- just-another-blog-frontend-react/src/smartComponents/SoftwareEngineering/SoftwareEngineering.js
+++ just-another-blog-frontend-react/src/smartComponents/SoftwareEngineering/SoftwareEngineering.js
@@ -7,7 +7,7 @@ class SoftwareEngineering extends Component {
     state = {
         SoftwareEngineeringArticles: [],
         // SoftwareEngineeringArticles: ""
-        WhichOptionIsChosen: "byTopic"
+        WhichOptionIsChosen: ""
     }
 
     componentDidMount () {
@@ -23,69 +23,105 @@ class SoftwareEngineering extends Component {
     }
 
     render () {
-        const introText = (
-            <p
-            style=
-            {{
-                textAlign: "left"
-            }}
-            >
-            So… more interested in becoming a master at Googling and 
-            copy pasting code from StackOverflow than everything else, huh?<br/>
-            Well I don't blame you! All joking aside I'm glad you came 😀<br/>
-            <br/>
-            With a new front end framework seemingly popping up every other year,<br/>
-            the classic "internships asking for ___ years of experience but 
-            to get experience I need an internship" conundrum,<br/>
-            job postings listing required qualifications that seem to 
-            better describe an entire business unit than an actual person,<br/>
-            and supply skyrocketing with astronomical increases in 
-            aspiring Computer Science majors and Boot Camp grads…<br/>
-            The world of software engineering seems as competitive and 
-            hard to navigate as it has ever been.<br/>
-            It might feel a bit daunting to do it alone 
-            (I know it was and still is for me).<br/>
-            <br/>
-            Now if I were to tell you I'd be your best navigator, I'd be lying through my teeth.<br/>
-            But what I can do is share with you what I've learned along the way, 
-            having been in the industry for a couple years now.<br/>
-            <br/>
-            And of course if I'm saying stuff you know is bogus... call me out on it!<br/>
-            I'd like to think of us more as vertices connected with a bidirectional edge in a graph 
-            than a parent node connected to a child node in a tree.<br/>
-            You get my flow?😏<br/>
-            <br/>
-            On the right-hand side you'll see a list of topics of which some you might be interested in.<br/>
-            Click on a topic to see all the posts that fall under it.<br/>
-            If you can't make up your mind, just click the by-date option 
-            to start with my most recent posts!<br/>
-            <br/>
-            Hope you find something you like!
-            </p>
-        );
-
-        const copyOfSoftwareEngineeringArticles = [...this.state.SoftwareEngineeringArticles]
-        const sweArticlesAsHTMLElements = copyOfSoftwareEngineeringArticles.map(
-            (article, i) => {
-            return <div key={i} dangerouslySetInnerHTML={{ __html: article}} />
-        });
-
+        let textBody = null;
         let listDependentOnWhichOptionIsChosen = null;
-        if (this.state.WhichOptionIsChosen === "byTopic"){
+        if (this.state.WhichOptionIsChosen === ""){
+            textBody = (
+                <p
+                style=
+                {{
+                    textAlign: "left"
+                }}
+                >
+                So… more interested in becoming a master at Googling and 
+                copy pasting code from StackOverflow than everything else, huh?<br/>
+                Well I don't blame you! All joking aside I'm glad you came 😀<br/>
+                <br/>
+                With a new front end framework seemingly popping up every other year,<br/>
+                the classic "internships asking for ___ years of experience but 
+                to get experience I need an internship" conundrum,<br/>
+                job postings listing required qualifications that seem to 
+                better describe an entire business unit than an actual person,<br/>
+                and supply skyrocketing with astronomical increases in 
+                aspiring Computer Science majors and Boot Camp grads…<br/>
+                The world of software engineering seems as competitive and 
+                hard to navigate as it has ever been.<br/>
+                It might feel a bit daunting to do it alone 
+                (I know it was and still is for me).<br/>
+                <br/>
+                Now if I were to tell you I'd be your best navigator, I'd be lying through my teeth.<br/>
+                But what I can do is share with you what I've learned along the way, 
+                having been in the industry for a couple years now.<br/>
+                <br/>
+                And of course if I'm saying stuff you know is bogus... call me out on it!<br/>
+                I'd like to think of us more as vertices connected with a bidirectional edge in a graph 
+                than a parent node connected to a child node in a tree.<br/>
+                You get my flow?😏<br/>
+                <br/>
+                On the right-hand side you'll see a list of topics of which some you might be interested in.<br/>
+                Click on a topic to see all the posts that fall under it.<br/>
+                If you can't make up your mind, just click the by-date option 
+                to start with my most recent posts!<br/>
+                <br/>
+                Hope you find something you like!
+                </p>
+            );    
             listDependentOnWhichOptionIsChosen = (
-            <p>
-                default to topics
-            </p>
+            <ul>
+                <li>Backend-Development
+                    <ul>
+                        <li>APIs</li>
+                        <li>Databases</li>
+                    </ul>
+                </li>
+                <li>Frontend-Development</li>
+                <li>Finding-A-Job</li>
+            </ul>
             );
         }
         else if (this.state.WhichOptionIsChosen === "byDate"){
+            const copyOfSoftwareEngineeringArticles = [...this.state.SoftwareEngineeringArticles]
+            const sweArticlesAsHTMLElements = copyOfSoftwareEngineeringArticles.map(
+                (article, i) => {
+                return <div key={i} dangerouslySetInnerHTML={{ __html: article}} />
+            });
+            textBody = sweArticlesAsHTMLElements;
+
             listDependentOnWhichOptionIsChosen = (
-            <p>
-                Switch to Date
-            </p>
+                // <p>
+                //     Switch to Date
+                // </p>
+                <ul>
+                    <li>2021</li>
+                    <li> 2020</li>
+                    <li>2019</li>
+                </ul>
+                );
+        }
+        else if (this.state.WhichOptionIsChosen === "byTopic"){
+            // const copyOfSoftwareEngineeringArticles = [...this.state.SoftwareEngineeringArticles]
+            // const sweArticlesAsHTMLElements = copyOfSoftwareEngineeringArticles.map(
+            //     (article, i) => {
+            //     return <div key={i} dangerouslySetInnerHTML={{ __html: article}} />
+            // });
+            // textBody = sweArticlesAsHTMLElements;
+            listDependentOnWhichOptionIsChosen = (
+            // <p>
+            //     default to topics
+            // </p>
+            <ul>
+                <li>Backend-Development
+                    <ul>
+                        <li>APIs</li>
+                        <li>Databases</li>
+                    </ul>
+                </li>
+                <li>Frontend-Development</li>
+                <li>Finding-A-Job</li>
+            </ul>
             );
         }
-            
+        
        
 
         return (
@@ -95,9 +131,7 @@ class SoftwareEngineering extends Component {
                 color: 'white'
             }}
         >
-            {/* {this.state.SoftwareEngineeringArticles} */}
-            {/* <div dangerouslySetInnerHTML={{ __html: this.state.SoftwareEngineeringArticles[1]}} /> */}
-            <div name="articlesAndContentList"
+            <div name="articlesOrIntroTextAndContentList"
             style=
             {{
                 display: "flex",
@@ -106,7 +140,7 @@ class SoftwareEngineering extends Component {
                 alignItems: "flex-start"
             }}
             >
-                <div name="articles"
+                <div name="articlesOrIntroText"
                 style=
                 {{
                     order: 1,
@@ -114,7 +148,7 @@ class SoftwareEngineering extends Component {
                     marginRight: 100
                 }}
                 >
-                    {sweArticlesAsHTMLElements}
+                    {textBody}
                 </div>
 
                 <div name="contentList"
@@ -143,25 +177,8 @@ class SoftwareEngineering extends Component {
                         >By Date</li>
                     </ul>
                     {listDependentOnWhichOptionIsChosen}
-                    {/* <ul>
-                        <li
-                        style=
-                        {{
-                            display: "inline"
-                        }}
-                        >Backend</li>
-                        <li
-                        style=
-                        {{
-                            display: "inline"
-                        }}
-                        >Frontend</li>
-                    </ul> */}
                 </div>  
             </div>
-            {/* {this.state.SoftwareEngineeringArticles[1]}
-            {this.state.SoftwareEngineeringArticles[0]} */}
-            {introText}
         </div>
         );
 
