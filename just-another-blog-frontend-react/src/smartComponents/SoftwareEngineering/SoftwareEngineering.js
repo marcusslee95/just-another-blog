1b7107d24f8992e7581b12de53064888e8ca36e1
Marcus Lee <marcusslee95@berkeley.edu>
2020-12-28 23:13:11 -0500

    frontend - added when click a year show the articles of that year functionality. Lots of changes made. One big thing was turning textBody that I show from normal variable to state variable

--- just-another-blog-frontend-react/src/smartComponents/SoftwareEngineering/SoftwareEngineering.js
+++ just-another-blog-frontend-react/src/smartComponents/SoftwareEngineering/SoftwareEngineering.js
@@ -8,7 +8,61 @@ class SoftwareEngineering extends Component {
         SoftwareEngineeringArticlesByDate: {},
         // SoftwareEngineeringArticles: [],
         // SoftwareEngineeringArticles: ""
-        WhichOptionIsChosen: ""
+        WhichOptionIsChosen: "",
+        textbody: (
+            <p
+            style=
+            {{
+                textAlign: "left"
+            }}
+            >
+            So… more interested in becoming a master at Googling and 
+            copy pasting code from StackOverflow than everything else, huh?<br/>
+            Well I don't blame you! All joking aside I'm glad you came 😀<br/>
+            <br/>
+            With a new front end framework seemingly popping up every other year,<br/>
+            the classic "internships asking for ___ years of experience but 
+            to get experience I need an internship" conundrum,<br/>
+            job postings listing required qualifications that seem to 
+            better describe an entire business unit than an actual person,<br/>
+            and supply skyrocketing with astronomical increases in 
+            aspiring Computer Science majors and Boot Camp grads…<br/>
+            The world of software engineering seems as competitive and 
+            hard to navigate as it has ever been.<br/>
+            It might feel a bit daunting to do it alone 
+            (I know it was and still is for me).<br/>
+            <br/>
+            Now if I were to tell you I'd be your best navigator, I'd be lying through my teeth.<br/>
+            But what I can do is share with you what I've learned along the way, 
+            having been in the industry for a couple years now.<br/>
+            <br/>
+            And of course if I'm saying stuff you know is bogus... call me out on it!<br/>
+            I'd like to think of us more as vertices connected with a bidirectional edge in a graph 
+            than a parent node connected to a child node in a tree.<br/>
+            You get my flow?😏<br/>
+            <br/>
+            On the right-hand side you'll see a list of topics of which some you might be interested in.<br/>
+            Click on a topic to see all the posts that fall under it.<br/>
+            If you can't make up your mind, just click the by-date option 
+            to start with my most recent posts!<br/>
+            <br/>
+            Hope you find something you like!
+            </p>
+        ),
+        listDependentOnWhichOptionIsChosen: (
+            <ul>
+                <li>Backend-Development
+                    <ul>
+                        <li>APIs</li>
+                        <li>Databases</li>
+                    </ul>
+                </li>
+                <li>Frontend-Development</li>
+                <li>Finding-A-Job</li>
+            </ul>
+            ),
+        ExistsToAvoidInfiniteRerender: false
+        // ClickedAListItem: false
     }
 
     componentDidMount () {
@@ -24,64 +78,131 @@ class SoftwareEngineering extends Component {
     }
 
     render () {
-        let textBody = null;
-        let listDependentOnWhichOptionIsChosen = null;
-        if (this.state.WhichOptionIsChosen === ""){
-            textBody = (
-                <p
-                style=
-                {{
-                    textAlign: "left"
-                }}
-                >
-                So… more interested in becoming a master at Googling and 
-                copy pasting code from StackOverflow than everything else, huh?<br/>
-                Well I don't blame you! All joking aside I'm glad you came 😀<br/>
-                <br/>
-                With a new front end framework seemingly popping up every other year,<br/>
-                the classic "internships asking for ___ years of experience but 
-                to get experience I need an internship" conundrum,<br/>
-                job postings listing required qualifications that seem to 
-                better describe an entire business unit than an actual person,<br/>
-                and supply skyrocketing with astronomical increases in 
-                aspiring Computer Science majors and Boot Camp grads…<br/>
-                The world of software engineering seems as competitive and 
-                hard to navigate as it has ever been.<br/>
-                It might feel a bit daunting to do it alone 
-                (I know it was and still is for me).<br/>
-                <br/>
-                Now if I were to tell you I'd be your best navigator, I'd be lying through my teeth.<br/>
-                But what I can do is share with you what I've learned along the way, 
-                having been in the industry for a couple years now.<br/>
-                <br/>
-                And of course if I'm saying stuff you know is bogus... call me out on it!<br/>
-                I'd like to think of us more as vertices connected with a bidirectional edge in a graph 
-                than a parent node connected to a child node in a tree.<br/>
-                You get my flow?😏<br/>
-                <br/>
-                On the right-hand side you'll see a list of topics of which some you might be interested in.<br/>
-                Click on a topic to see all the posts that fall under it.<br/>
-                If you can't make up your mind, just click the by-date option 
-                to start with my most recent posts!<br/>
-                <br/>
-                Hope you find something you like!
-                </p>
-            );    
-            listDependentOnWhichOptionIsChosen = (
-            <ul>
-                <li>Backend-Development
-                    <ul>
-                        <li>APIs</li>
-                        <li>Databases</li>
-                    </ul>
-                </li>
-                <li>Frontend-Development</li>
-                <li>Finding-A-Job</li>
-            </ul>
-            );
-        }
-        else if (this.state.WhichOptionIsChosen === "byDate"){
-            //1. show all the articles by....  making them displayable in html aka. an array of divs
+        // let listDependentOnWhichOptionIsChosen = null;
+        // if (this.state.WhichOptionIsChosen === ""){
+        //     this.setState({textbody: (
+        //         <p
+        //         style=
+        //         {{
+        //             textAlign: "left"
+        //         }}
+        //         >
+        //         So… more interested in becoming a master at Googling and 
+        //         copy pasting code from StackOverflow than everything else, huh?<br/>
+        //         Well I don't blame you! All joking aside I'm glad you came 😀<br/>
+        //         <br/>
+        //         With a new front end framework seemingly popping up every other year,<br/>
+        //         the classic "internships asking for ___ years of experience but 
+        //         to get experience I need an internship" conundrum,<br/>
+        //         job postings listing required qualifications that seem to 
+        //         better describe an entire business unit than an actual person,<br/>
+        //         and supply skyrocketing with astronomical increases in 
+        //         aspiring Computer Science majors and Boot Camp grads…<br/>
+        //         The world of software engineering seems as competitive and 
+        //         hard to navigate as it has ever been.<br/>
+        //         It might feel a bit daunting to do it alone 
+        //         (I know it was and still is for me).<br/>
+        //         <br/>
+        //         Now if I were to tell you I'd be your best navigator, I'd be lying through my teeth.<br/>
+        //         But what I can do is share with you what I've learned along the way, 
+        //         having been in the industry for a couple years now.<br/>
+        //         <br/>
+        //         And of course if I'm saying stuff you know is bogus... call me out on it!<br/>
+        //         I'd like to think of us more as vertices connected with a bidirectional edge in a graph 
+        //         than a parent node connected to a child node in a tree.<br/>
+        //         You get my flow?😏<br/>
+        //         <br/>
+        //         On the right-hand side you'll see a list of topics of which some you might be interested in.<br/>
+        //         Click on a topic to see all the posts that fall under it.<br/>
+        //         If you can't make up your mind, just click the by-date option 
+        //         to start with my most recent posts!<br/>
+        //         <br/>
+        //         Hope you find something you like!
+        //         </p>
+        //     )
+        // });
+        //     listDependentOnWhichOptionIsChosen = (
+        //     <ul>
+        //         <li>Backend-Development
+        //             <ul>
+        //                 <li>APIs</li>
+        //                 <li>Databases</li>
+        //             </ul>
+        //         </li>
+        //         <li>Frontend-Development</li>
+        //         <li>Finding-A-Job</li>
+        //     </ul>
+        //     );
+        // }
+        // else if (this.state.WhichOptionIsChosen === "byDate" ){
+        //     //1. show all the articles by....  making them displayable in html aka. an array of divs
+        //     const arrOfArticles = Object.values(this.state.SoftwareEngineeringArticlesByDate);
+        //     console.log(arrOfArticles);
+        //     let sweArticlesAsHTMLElements = []
+        //     arrOfArticles.map(
+        //         (arrOfArticlesForOneYear, i) => {
+        //         arrOfArticlesForOneYear.map(
+        //             (article, i) => {
+        //                 sweArticlesAsHTMLElements.push(<div key={article.substring(1, 20)} dangerouslySetInnerHTML={{ __html: article}} />)
+        //         });
+        //     });
+        //     // textBody = sweArticlesAsHTMLElements;
+        //     this.setState({textbody: sweArticlesAsHTMLElements});
+
+        //     //2. generate the content list 
+        //     const arrOfYears = Object.keys(this.state.SoftwareEngineeringArticlesByDate);
+        //     console.log(arrOfYears);
+        //     const listItems = arrOfYears.map(
+        //         (year, i) => {
+        //         return <li key={i}
+        //         onClick={() => {
+        //             // alert("Placeholder for showing articles of year " + year);
+        //             const copyOfSoftwareEngineeringArticlesForThatYear = [...this.state.SoftwareEngineeringArticlesByDate[year]]
+        //             console.log(copyOfSoftwareEngineeringArticlesForThatYear);
+        //             const sweArticlesAsHTMLElements2 = 
+        //             copyOfSoftwareEngineeringArticlesForThatYear.map(
+        //                 (article2, i) => {
+        //                 return <div key={i} dangerouslySetInnerHTML={{ __html: article2}} />
+        //             });
+        //             console.log(sweArticlesAsHTMLElements2);
+        //             // textBody = sweArticlesAsHTMLElements2;
+        //             this.setState({textbody: sweArticlesAsHTMLElements2});
+        //             // console.log(textBody);
+        //         }}
+        //         >{year}</li>
+        //     });
+        //     listDependentOnWhichOptionIsChosen = (
+        //         <ul>
+        //             {listItems}
+        //         </ul>
+        //         );
+        // }
+        // else if (this.state.WhichOptionIsChosen === "byTopic"){
+        //     // const copyOfSoftwareEngineeringArticles = [...this.state.SoftwareEngineeringArticles]
+        //     // const sweArticlesAsHTMLElements = copyOfSoftwareEngineeringArticles.map(
+        //     //     (article, i) => {
+        //     //     return <div key={i} dangerouslySetInnerHTML={{ __html: article}} />
+        //     // });
+        //     // textBody = sweArticlesAsHTMLElements;
+        //     listDependentOnWhichOptionIsChosen = (
+        //     // <p>
+        //     //     default to topics
+        //     // </p>
+        //     <ul>
+        //         <li>Backend-Development
+        //             <ul>
+        //                 <li>APIs</li>
+        //                 <li>Databases</li>
+        //             </ul>
+        //         </li>
+        //         <li>Frontend-Development</li>
+        //         <li>Finding-A-Job</li>
+        //     </ul>
+        //     );
+        // }
+        if (this.state.WhichOptionIsChosen === "byDate" && 
+        this.state.ExistsToAvoidInfiniteRerender === false){
+                //1. show all the articles by....  making them displayable in html aka. an array of divs
             const arrOfArticles = Object.values(this.state.SoftwareEngineeringArticlesByDate);
             console.log(arrOfArticles);
             let sweArticlesAsHTMLElements = []
@@ -92,47 +213,38 @@ class SoftwareEngineering extends Component {
                         sweArticlesAsHTMLElements.push(<div key={article.substring(1, 20)} dangerouslySetInnerHTML={{ __html: article}} />)
                 });
             });
-            textBody = sweArticlesAsHTMLElements;
+            // textBody = sweArticlesAsHTMLElements;
+            this.setState({textbody: sweArticlesAsHTMLElements});
 
-            //2. generate the content list 
+                //2. generate the content list 
             const arrOfYears = Object.keys(this.state.SoftwareEngineeringArticlesByDate);
             console.log(arrOfYears);
             const listItems = arrOfYears.map(
                 (year, i) => {
                 return <li key={i}
                 onClick={() => {
-                    alert("Placeholder for showing articles of year " + year);
+                    // alert("Placeholder for showing articles of year " + year);
+                    const copyOfSoftwareEngineeringArticlesForThatYear = [...this.state.SoftwareEngineeringArticlesByDate[year]]
+                    console.log(copyOfSoftwareEngineeringArticlesForThatYear);
+                    const sweArticlesAsHTMLElements2 = 
+                    copyOfSoftwareEngineeringArticlesForThatYear.map(
+                        (article2, i) => {
+                        return <div key={i} dangerouslySetInnerHTML={{ __html: article2}} />
+                    });
+                    console.log(sweArticlesAsHTMLElements2);
+                    // textBody = sweArticlesAsHTMLElements2;
+                    this.setState({textbody: sweArticlesAsHTMLElements2});
+                    // console.log(textBody);
                 }}
                 >{year}</li>
             });
-            listDependentOnWhichOptionIsChosen = (
-                <ul>
-                    {listItems}
-                </ul>
-                );
-        }
-        else if (this.state.WhichOptionIsChosen === "byTopic"){
-            // const copyOfSoftwareEngineeringArticles = [...this.state.SoftwareEngineeringArticles]
-            // const sweArticlesAsHTMLElements = copyOfSoftwareEngineeringArticles.map(
-            //     (article, i) => {
-            //     return <div key={i} dangerouslySetInnerHTML={{ __html: article}} />
-            // });
-            // textBody = sweArticlesAsHTMLElements;
-            listDependentOnWhichOptionIsChosen = (
-            // <p>
-            //     default to topics
-            // </p>
+            this.setState({listDependentOnWhichOptionIsChosen:  (
             <ul>
-                <li>Backend-Development
-                    <ul>
-                        <li>APIs</li>
-                        <li>Databases</li>
-                    </ul>
-                </li>
-                <li>Frontend-Development</li>
-                <li>Finding-A-Job</li>
+                {listItems}
             </ul>
-            );
+            ),
+            ExistsToAvoidInfiniteRerender: true
+            });
         }
         
        
@@ -161,7 +273,7 @@ class SoftwareEngineering extends Component {
                     marginRight: 100
                 }}
                 >
-                    {textBody}
+                    {this.state.textbody}
                 </div>
 
                 <div name="contentList"
@@ -189,7 +301,7 @@ class SoftwareEngineering extends Component {
                         className={this.state.WhichOptionIsChosen === "byDate" ? cssClass.active : null}
                         >By Date</li>
                     </ul>
-                    {listDependentOnWhichOptionIsChosen}
+                    {this.state.listDependentOnWhichOptionIsChosen}
                 </div>  
             </div>
         </div>
