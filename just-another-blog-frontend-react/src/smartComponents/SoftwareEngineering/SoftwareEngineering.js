0a1c39004052193830f0b2a67a96128519baedc4
Marcus Lee <marcusslee95@berkeley.edu>
2020-12-29 13:29:08 -0500

    started working on bytopic. successfully sent request to backend to getsweArticlesByTopic endpoint and got back placeholder response and assigned placeholder response value to swearticlesbytopic state variable

--- just-another-blog-frontend-react/src/smartComponents/SoftwareEngineering/SoftwareEngineering.js
+++ just-another-blog-frontend-react/src/smartComponents/SoftwareEngineering/SoftwareEngineering.js
@@ -61,12 +61,13 @@ class SoftwareEngineering extends Component {
                 <li>Finding-A-Job</li>
             </ul>
             ),
-        ExistsToAvoidInfiniteRerender: false
+        ExistsToAvoidInfiniteRerender: false,
+        SoftwareEngineeringArticlesByTopic: {}
         // ClickedAListItem: false
     }
 
     componentDidMount () {
-        axios.get( 'http://localhost:4000/getSoftwareEngineeringArticles' )
+        axios.get( 'http://localhost:4000/getSoftwareEngineeringArticlesByDate' )
             .then( response => {
                 console.log( response.data );
                 this.setState({SoftwareEngineeringArticlesByDate: response.data});
@@ -75,52 +76,19 @@ class SoftwareEngineering extends Component {
                 // console.log(error);
                 this.setState({error: true});
             });
+        axios.get( 'http://localhost:4000/getSoftwareEngineeringArticlesByTopic' )
+            .then( response => {
+                // console.log( response.data );
+                alert(response.data )
+                this.setState({SoftwareEngineeringArticlesByTopic: response.data});
+            } )
+            .catch(error => {
+                // console.log(error);
+                this.setState({error: true});
+            });
     }
 
     render () {
-        // let listDependentOnWhichOptionIsChosen = null;
-        // if (this.state.WhichOptionIsChosen === ""){
-        //     this.setState({textbody: (
-        //         <p
-        //         style=
-        //         {{
-        //             textAlign: "left"
-        //         }}
-        //         >
-        //         So… more interested in becoming a master at Googling and 
-        //         copy pasting code from StackOverflow than everything else, huh?<br/>
-        //         Well I don't blame you! All joking aside I'm glad you came 😀<br/>
-        //         <br/>
-        //         With a new front end framework seemingly popping up every other year,<br/>
-        //         the classic "internships asking for ___ years of experience but 
-        //         to get experience I need an internship" conundrum,<br/>
-        //         job postings listing required qualifications that seem to 
-        //         better describe an entire business unit than an actual person,<br/>
-        //         and supply skyrocketing with astronomical increases in 
-        //         aspiring Computer Science majors and Boot Camp grads…<br/>
-        //         The world of software engineering seems as competitive and 
-        //         hard to navigate as it has ever been.<br/>
-        //         It might feel a bit daunting to do it alone 
-        //         (I know it was and still is for me).<br/>
-        //         <br/>
-        //         Now if I were to tell you I'd be your best navigator, I'd be lying through my teeth.<br/>
-        //         But what I can do is share with you what I've learned along the way, 
-        //         having been in the industry for a couple years now.<br/>
-        //         <br/>
-        //         And of course if I'm saying stuff you know is bogus... call me out on it!<br/>
-        //         I'd like to think of us more as vertices connected with a bidirectional edge in a graph 
-        //         than a parent node connected to a child node in a tree.<br/>
-        //         You get my flow?😏<br/>
-        //         <br/>
-        //         On the right-hand side you'll see a list of topics of which some you might be interested in.<br/>
-        //         Click on a topic to see all the posts that fall under it.<br/>
-        //         If you can't make up your mind, just click the by-date option 
-        //         to start with my most recent posts!<br/>
-        //         <br/>
-        //         Hope you find something you like!
-        //         </p>
-        //     )
-        // });
         //     listDependentOnWhichOptionIsChosen = (
         //     <ul>
         //         <li>Backend-Development
