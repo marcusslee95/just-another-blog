ee7be250f6b146e8c796688ae5a97b7bab8adb31
Marcus Lee <marcusslee95@berkeley.edu>
2021-01-07 21:29:15 -0800

    frontend - changed so that dont make any changes to content list when componentdidmount - aka when component first gets created - only change content list when click either header. Problem to solve next, content list doesn't change back if i click bydate to bytopic back to bydate and also bytopic to bydatae back to bytopic

--- just-another-blog-frontend-react/src/smartComponents/SoftwareEngineering/SoftwareEngineering.js
+++ just-another-blog-frontend-react/src/smartComponents/SoftwareEngineering/SoftwareEngineering.js
@@ -80,103 +80,103 @@ class SoftwareEngineering extends Component {
             .then( response => {
                 // console.log( response.data );
                 // alert(response.data )
-                //B4: make the initial content list which shows by topic topics - only first level
-                    //generate the content list 
-                const arrOfTopicsFirstLvl = Object.keys(response.data);
-                console.log(arrOfTopicsFirstLvl);
-                const listItems = arrOfTopicsFirstLvl.map(
-                    (topic, i) => {
-                    return <li key={i}
+                // //B4: make the initial content list which shows by topic topics - only first level
+                //     //generate the content list 
+                // const arrOfTopicsFirstLvl = Object.keys(response.data);
+                // console.log(arrOfTopicsFirstLvl);
+                // const listItems = arrOfTopicsFirstLvl.map(
+                //     (topic, i) => {
+                //     return <li key={i}
 
-                    onClick={() => {
-                        //B4: show all articles related to that topic
-                        const objectRepresentingThatTopic = response.data[topic]
-                        // console.log(objectRepresentingThatTopic);
-                        const arrOfSubtopics = Object.keys(objectRepresentingThatTopic)
-                        // console.log(arrOfSubtopics);
-                        let sweArticlesRelatedToThisTopic = [];
-                        arrOfSubtopics.forEach(
-                            (subtopic, index) => {
-                                const arrOfArticlesForThisSubtopic = objectRepresentingThatTopic[subtopic]
-                                arrOfArticlesForThisSubtopic.forEach(
-                                    (article, indieeee) => {
-                                        const articleFormatThatReactCanRecognizeItsHTMLOtherwiseJustReadsAsNormalString = 
-                                        <div key={article.substring(1, 20)} dangerouslySetInnerHTML={{ __html: article}} />
-                                    sweArticlesRelatedToThisTopic.push(articleFormatThatReactCanRecognizeItsHTMLOtherwiseJustReadsAsNormalString)     
-                                });
-                        });
-                        // console.log(sweArticlesRelatedToThisTopic);
-                        this.setState({textbody: sweArticlesRelatedToThisTopic});
-                        //AFTER: show all articles related to that topic
+                //     onClick={() => {
+                //         //B4: show all articles related to that topic
+                //         const objectRepresentingThatTopic = response.data[topic]
+                //         // console.log(objectRepresentingThatTopic);
+                //         const arrOfSubtopics = Object.keys(objectRepresentingThatTopic)
+                //         // console.log(arrOfSubtopics);
+                //         let sweArticlesRelatedToThisTopic = [];
+                //         arrOfSubtopics.forEach(
+                //             (subtopic, index) => {
+                //                 const arrOfArticlesForThisSubtopic = objectRepresentingThatTopic[subtopic]
+                //                 arrOfArticlesForThisSubtopic.forEach(
+                //                     (article, indieeee) => {
+                //                         const articleFormatThatReactCanRecognizeItsHTMLOtherwiseJustReadsAsNormalString = 
+                //                         <div key={article.substring(1, 20)} dangerouslySetInnerHTML={{ __html: article}} />
+                //                     sweArticlesRelatedToThisTopic.push(articleFormatThatReactCanRecognizeItsHTMLOtherwiseJustReadsAsNormalString)     
+                //                 });
+                //         });
+                //         // console.log(sweArticlesRelatedToThisTopic);
+                //         this.setState({textbody: sweArticlesRelatedToThisTopic});
+                //         //AFTER: show all articles related to that topic
 
-                        //B4: change content list to show subtopicss of topic you just clicked
-                        //APPROACH:
-                        //1. for all the topics other than the one that was clicked, just use previous value
-                        //2. only for topic that was clicked instead of using previous value, create value that has subtopics too
-                            //2a. we can know which topic was the clicked topic if we loop through all the list items and compare their key to current li key
-                            //2b. now that we can identify clicked topic, when reach it in the loop create 2nd level ul that contains all the subtopics of that topic as lis
-                        // let changedContentList = [...listItems]
-                        let changedContentList = []
-                        listItems.forEach(
-                            (topic, ind) => {
-                                if (i === ind){//we're at the topic that we clicked 
-                                const subtopicsAsHTMLListItems = arrOfSubtopics.map((subtopic, indexxx) => {
-                                        return <li key={indexxx}
-                                        onClick={() => {
-                                            // alert(subtopic)
-                                            //B4: show all articles related to that subtopic
-                                            const sweArticlesRelatedToThisSubtopic = [];
-                                            const arrOfArtInSubtopic = objectRepresentingThatTopic[subtopic]
-                                            arrOfArtInSubtopic.forEach(
-                                                (article, indieeeexxxxxx) => {
-                                                    const articleFormatThatReactCanRecognizeItsHTMLOtherwiseJustReadsAsNormalString = 
-                                                    <div key={article.substring(1, 20)} dangerouslySetInnerHTML={{ __html: article}} />
-                                                    // sweArticlesRelatedToThisSubtopic.push(article)
-                                                    sweArticlesRelatedToThisSubtopic.push(articleFormatThatReactCanRecognizeItsHTMLOtherwiseJustReadsAsNormalString)     
-                                            });
-                                            this.setState({textbody: sweArticlesRelatedToThisSubtopic});
-                                            //AFTER: show all articles related to that subtopic
-                                        }}
-                                            >{subtopic}</li>
-                                    }
-                                )
-                                    let rv = (                                       
-                                            <li key={topic}>{topic}
-                                            <ul>
-                                                {subtopicsAsHTMLListItems}
-                                            </ul>
-                                            </li>                                            
-                                        );
-                                    changedContentList.push(rv) 
-                                }
-                                else {
-                                    changedContentList.push(listItems[ind])    
-                                }
-                        });
-                        console.log(changedContentList);
-                        // this.setState({listDependentOnWhichOptionIsChosen: <li>gfxgg</li>});
-                        this.setState({listDependentOnWhichOptionIsChosen: (
-                            <ul>
-                                {changedContentList}
-                            </ul>
-                            ),
-                            //B4: highlight By Topic List Header
-                            WhichOptionIsChosen: "byTopic"
-                            //AFTER: highlight By Topic List Header
-                        });
-                        //AFTER: change content list to show subtopicss of topic you just clicked
-                    }}
+                //         //B4: change content list to show subtopicss of topic you just clicked
+                //         //APPROACH:
+                //         //1. for all the topics other than the one that was clicked, just use previous value
+                //         //2. only for topic that was clicked instead of using previous value, create value that has subtopics too
+                //             //2a. we can know which topic was the clicked topic if we loop through all the list items and compare their key to current li key
+                //             //2b. now that we can identify clicked topic, when reach it in the loop create 2nd level ul that contains all the subtopics of that topic as lis
+                //         // let changedContentList = [...listItems]
+                //         let changedContentList = []
+                //         listItems.forEach(
+                //             (topic, ind) => {
+                //                 if (i === ind){//we're at the topic that we clicked 
+                //                 const subtopicsAsHTMLListItems = arrOfSubtopics.map((subtopic, indexxx) => {
+                //                         return <li key={indexxx}
+                //                         onClick={() => {
+                //                             // alert(subtopic)
+                //                             //B4: show all articles related to that subtopic
+                //                             const sweArticlesRelatedToThisSubtopic = [];
+                //                             const arrOfArtInSubtopic = objectRepresentingThatTopic[subtopic]
+                //                             arrOfArtInSubtopic.forEach(
+                //                                 (article, indieeeexxxxxx) => {
+                //                                     const articleFormatThatReactCanRecognizeItsHTMLOtherwiseJustReadsAsNormalString = 
+                //                                     <div key={article.substring(1, 20)} dangerouslySetInnerHTML={{ __html: article}} />
+                //                                     // sweArticlesRelatedToThisSubtopic.push(article)
+                //                                     sweArticlesRelatedToThisSubtopic.push(articleFormatThatReactCanRecognizeItsHTMLOtherwiseJustReadsAsNormalString)     
+                //                             });
+                //                             this.setState({textbody: sweArticlesRelatedToThisSubtopic});
+                //                             //AFTER: show all articles related to that subtopic
+                //                         }}
+                //                             >{subtopic}</li>
+                //                     }
+                //                 )
+                //                     let rv = (                                       
+                //                             <li key={topic}>{topic}
+                //                             <ul>
+                //                                 {subtopicsAsHTMLListItems}
+                //                             </ul>
+                //                             </li>                                            
+                //                         );
+                //                     changedContentList.push(rv) 
+                //                 }
+                //                 else {
+                //                     changedContentList.push(listItems[ind])    
+                //                 }
+                //         });
+                //         console.log(changedContentList);
+                //         // this.setState({listDependentOnWhichOptionIsChosen: <li>gfxgg</li>});
+                //         this.setState({listDependentOnWhichOptionIsChosen: (
+                //             <ul>
+                //                 {changedContentList}
+                //             </ul>
+                //             ),
+                //             //B4: highlight By Topic List Header
+                //             WhichOptionIsChosen: "byTopic"
+                //             //AFTER: highlight By Topic List Header
+                //         });
+                //         //AFTER: change content list to show subtopicss of topic you just clicked
+                //     }}
 
-                    >{topic}</li>
-                });
-                console.log(listItems);
-                //AFTER: make the initial content list which shows by topic topics - only first level
+                //     >{topic}</li>
+                // });
+                // console.log(listItems);
+                // //AFTER: make the initial content list which shows by topic topics - only first level
                 this.setState({SoftwareEngineeringArticlesByTopic: response.data,
-                    listDependentOnWhichOptionIsChosen:  (
-                        <ul>
-                            {listItems}
-                        </ul>
-                        )
+                    // listDependentOnWhichOptionIsChosen:  (
+                    //     <ul>
+                    //         {listItems}
+                    //     </ul>
+                    //     )
                 });
             } )
             .catch(error => {
